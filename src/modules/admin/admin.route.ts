8ffca8301d413d8592f09284820eb1dea2b868d6
lukmanxpert <lukmanmiah2004@gmail.com>
2026-02-13 21:39:29 +0600

    added async handler all routes

--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -1,12 +1,29 @@
 import { Router } from "express";
 import { requireRole } from "../../middlewares/requireRole";
 import { adminController } from "./admin.controller";
+import { asyncHandler } from "../../helpers/asyncHandler";
 
 const router = Router();
 
-router.get("/users", requireRole("ADMIN"), adminController.getAllUsers);
-router.get("/providers", requireRole("ADMIN"), adminController.getAllProviders);
-router.get("/orders", requireRole("ADMIN"), adminController.getAllOrders);
-router.patch("/user/:userId", requireRole("ADMIN"), adminController.updateUserAdmin);
+router.get(
+  "/users",
+  requireRole("ADMIN"),
+  asyncHandler(adminController.getAllUsers),
+);
+router.get(
+  "/providers",
+  requireRole("ADMIN"),
+  asyncHandler(adminController.getAllProviders),
+);
+router.get(
+  "/orders",
+  requireRole("ADMIN"),
+  asyncHandler(adminController.getAllOrders),
+);
+router.patch(
+  "/user/:userId",
+  requireRole("ADMIN"),
+  asyncHandler(adminController.updateUserAdmin),
+);
 
 export const adminRouter: Router = router;
